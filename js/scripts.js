0e59cb09b89ed1c630f6686013f925c547351b10
Clifford Grimmell and Jahan Walsh <clifford@>
2017-02-15 15:08:16 -0800

    add info id to measurement groups, fix variable names to ouptput result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,67 @@
-function gallonsLiter (gallon) {
-  return gallon * 3.78;
+function gallonsLiter (gal) {
+  return gal * 3.78;
 };
 
-function litersGal (liter) {
-  return liter * 0.26;
+function litersGal (lit) {
+  return lit * 0.26;
 };
 
-function teaspoon (tablespoon) {
-  return tablespoon * 3;
+function teaspoon (tab) {
+  return tab * 3;
 };
 
-function tablespoon (teaspoon) {
-  return teaspoon * 0.333333;
+function tablespoon (tea) {
+  return tea * 0.333333;
 };
 
-function oz (cup) {
-  return cup * 8;
+function oz (cup1) {
+  return cup1 * 8;
 };
 
-function cup (oz) {
-  return oz * 0.125;
+function cup (oz1) {
+  return oz1 * 0.125;
 };
 
 $(function(){
   $("form#gallonsLiter").submit(function(event) {
     //debugger;
     var gallon = parseInt($("input#gL").val());
-    var result1 = gallonsLiter("gallon");
+    var result1 = gallonsLiter(gallon);
     $("#output1").text(result1);
     event.preventDefault();
-    console.log(result1);
   });
 
   $("form#litersGal").submit(function(event) {
-    var number = parseInt($("#lG").val());
-    var result2 = litersGal("input#lG");
+    var liter = parseInt($("input#lG").val());
+    var result2 = litersGal(liter);
     $("#output2").text(result2);
     event.preventDefault();
   });
 
   $("form#teaspoon").submit(function(event) {
-    var number = parseInt($("#tSpoon").val());
-    var result3 = teaspoon("input#tSpoon");
+    var teasp = parseInt($("input#tSpoon").val());
+    var result3 = teaspoon(teasp);
     $("#output3").text(result3);
     event.preventDefault();
   });
 
   $("form#tablespoon").submit(function(event) {
-    var number = parseInt($("#table").val());
-    var result4 = tablespoon("input#table");
+    var tblsp = parseInt($("input#table").val());
+    var result4 = tablespoon(tblsp);
     $("#output4").text(result4);
     event.preventDefault();
   });
 
   $("form#ounce").submit(function(event) {
-    var number = parseInt($("#oz").val());
-    var result5 = oz("input#oz");
+    var ounce1 = parseInt($("input#oz").val());
+    var result5 = oz(ounce1);
     $("#output5").text(result5);
     event.preventDefault();
   });
 
   $("form#cups").submit(function(event) {
-    var number = parseInt($("#cup").val());
-    var result6 = cup("input#cup");
+    var cup1 = parseInt($("#cup").val());
+    var result6 = cup(cup1);
     $("#output6").text(result6);
     event.preventDefault();
 
